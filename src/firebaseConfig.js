95d6b1810285e8f9844dcc0aaf72ae893b76a4ca
alyson-b69 <alyson.bernabeu@gmail.com>
2020-08-03 11:43:02 +0200

    Add env variables

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAdn5Oa0ZurI6okw12B6fKU0xWqJl_nltw",
-    authDomain: "react-firebase-blog-c62c5.firebaseapp.com",
-    databaseURL: "https://react-firebase-blog-c62c5.firebaseio.com",
-    projectId: "react-firebase-blog-c62c5",
-    storageBucket: "react-firebase-blog-c62c5.appspot.com",
-    messagingSenderId: "300893299289",
-    appId: "1:300893299289:web:a679843d3c13183982c748"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig
+
